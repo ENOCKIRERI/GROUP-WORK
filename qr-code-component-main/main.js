79c4d3eb5cb8192ad88e9794ff7e43a038b84d4c
ENOCKIRERI <enockireri654@gmail.com>
2023-02-03 08:49:30 +0300

    add the files

---
+++ qr-code-component-main/main.js
@@ -0,0 +1,15 @@
+//function removeDuplicates(array) {
+   // return [...new Set(array)];
+   function removeDuplicates(array) {
+    return [...new Set(array)];
+  }
+  
+   let array1=[1,2,3,3,4,5,5];
+   let Array2=removeDuplicates(array1);
+   console.log(Array2);
+
+   function removeDuplicatesAndCaseSensitive(arr) {
+   let names=[Enock,enock,Jillian,jillian,jospeph,Joseph];
+   let uniquenames = [names];
+   console.log(uniquenames);
+   }
